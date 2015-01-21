585052a547de5d8e42df3c80d103f13787cf90ef
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2015-01-21 18:44:45 +0100

    Add support for ZES Zimmer LMG670 Power Meter.

---
+++ lmg670.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+
+# lmg670.py
+#
+# Implement interface to ZES Zimmer LMG670 Power Meter
+#
+# 2015-01, Jan de Cuveland
+
+import socket
+
+EOS = "\n"
+TIMEOUT = 2
+
+class lmg_socket:
+    def __init__(self, host = ""):
+        self._s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if host != "":
+            self.connect(host, 5025)
+
+    def connect(self, host, port):
+        self._s.connect((host, port))
+        self._host = host
+        self._port = port
+        self._s.settimeout(TIMEOUT)
+        
+    def send(self, msg):
+        self._s.sendall(msg + EOS)
+
+    def recv_str(self):
+        response = ""
+        while response[-len(EOS):] != EOS:
+            try:
+                response += self._s.recv(4096)
+            except socket.timeout as e:
+                print "error:", e
+                return ""
+        return response[:-len(EOS)]
+
+    def send_cmd(self, cmd):
+        result = self.query(cmd + ";*opc?")
+        if result != "1":
+            print "opc returned unexpected value:", result
+
+    def send_brk(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self._host, self._port + 1))
+        s.settimeout(TIMEOUT)
+        s.sendall("break\n")
+        response = ""
+        while response[-len(EOS):] != EOS:
+            try:
+                response += s.recv(256)
+            except socket.timeout as e:
+                print "error:", e
+                s.close()
+                return False
+        s.close()
+        return (response[:-len(EOS)] == "0 ok")
+
+    def query(self, msg):
+        self.send(msg)
+        return self.recv_str()
+
+    def close(self):
+        self._s.close()
+    
+    def __del__(self):
+        self.close()
+
+
+class lmg670(lmg_socket):
+    _short_commands_enabled = False
+
+    def reset(self):
+        self.send_brk()
+        self._short_commands_enabled = False
+        self.send_cmd("*rst;*cls")
+
+    def goto_short_commands(self):
+        if not self._short_commands_enabled:
+            self.send("*zlang short")
+        self._short_commands_enabled = True
+
+    def goto_scpi_commands(self):
+        if self._short_commands_enabled:
+            self.send("*zlang scpi")
+        self._short_commands_enabled = False
+
+    def send_short(self, msg):
+        self.goto_short_commands()
+        self.send(msg)
+
+    def send_scpi(self, msg):
+        self.goto_scpi_commands()
+        self.send(msg)
+
+    def send_short_cmd(self, cmd):
+        self.goto_short_commands()
+        self.send_cmd(cmd)
+
+    def send_scpi_cmd(self, cmd):
+        self.goto_scpi_commands()
+        self.send_cmd(cmd)
+
+    def query_short(self, msg):
+        self.goto_short_commands()
+        return self.query(msg)
+
+    def query_scpi(self, msg):
+        self.goto_scpi_commands()
+        return self.query(msg)
+
+    def goto_local(self):
+        self.send("gtl")
+
+    def read_id(self):
+        return self.query("*idn?").split(",")
+
+    def read_errors(self):
+        return self.query_scpi("syst:err:all?")
+
+    def set_ranges(self, current, voltage):
+        for c in range(1, 8):
+            cmd = "iauto{0} 0;uauto{0} 0;irng{0} {1};urng{0} {2}".format(c, current, voltage)
+            self.send_short_cmd(cmd)
+
+    def select_values(self, values):
+        self.send_short('actn;' + "?;".join(values) + "?")
+
+    def read_raw_values(self):
+        return self.recv_str().split(";")
+        
+    def read_float_values(self):
+        values_raw = self.read_raw_values()
+        return [ float(x) for x in values_raw ]
+
+    def cont_on(self):
+        self.send_short("cont on")
+
+    def cont_off(self):
+        self.send_short("cont off")
+        
+    def disconnect(self):
+        self.read_errors()
+        self.goto_local()
---
+++ powerlog670.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+# powerlog670.py
+#
+# Log measured values from ZES Zimmer LMG670 Power Meter
+#
+# 2015-01, Jan de Cuveland
+
+import argparse, sys, lmg670
+
+VAL = "tsnorm durnorm utrms itrms udc idc ucf icf uff iff p pf fcyc".split()
+VAL = [v + str(c) for c in range(1, 7) for v in VAL]
+
+def main():
+    parser = argparse.ArgumentParser(
+        description = "Log measured values from ZES Zimmer LMG670 Power Meter")
+    parser.add_argument("host", help = "Hostname of LMG670")
+    parser.add_argument("logfile", help = "Log file name")
+    args = parser.parse_args()
+
+    print "connecting to", args.host
+    lmg = lmg670.lmg670(args.host)
+
+    print "performing device reset"
+    lmg.reset()
+
+    print "device found:", lmg.read_id()[1]
+
+    print "setting up device"
+    lmg.set_ranges(10.0, 250.0)
+    lmg.select_values(VAL)
+
+    log = open(args.logfile, "w");
+    i = 0
+    try:
+        lmg.cont_on()
+        log.write("# " + " ".join(VAL) + "\n")
+        print "writing values to", args.logfile
+        print "press CTRL-C to stop"
+        while True:
+            data = lmg.read_raw_values()
+            i += 1
+            sys.stdout.write("\r{0}".format(i))
+            sys.stdout.flush()
+            log.write(" ".join(data) + "\n")
+    except KeyboardInterrupt:
+        print
+
+    lmg.cont_off()
+    log.close()
+
+    lmg.disconnect()
+    print "done,", i, "measurements written"
+
+
+if __name__ == "__main__":
+    main()
