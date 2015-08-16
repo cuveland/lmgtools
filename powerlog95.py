23badaddea2ae0ea9be1ad759d28cdc0d52ee3a0
David Rohr <drohr@jwdt.org>
2015-08-16 16:54:38 +0200

    Add option to set reading interval for LMG670, add option for verbose output that prints the reading to console, merge in some options added by Matthias Bach for his tests of LQCD code with LMG95

--- powerlog95.py
+++ powerlog95.py
@@ -17,6 +17,11 @@ def main():
     parser.add_argument("logfile", help = "Log file name")
     parser.add_argument("-p", "--port", dest = "port", type = int, default=2001,
                         help = "TCP port of RS232-Ethernet converter")
+    parser.add_argument("-v", "--verbose", dest = "verbose", type = int, default=0, help = "Verbose")
+    parser.add_argument("-l", "--lowpass", dest = "lowpass", action="store_true", default=False,
+                        help = "Enable 60 Hz low pass filter")
+    parser.add_argument("-i", "--interval", dest="interval", type=float, default=0.5,
+                        help = "Measurement interval in seconds")
     args = parser.parse_args()
 
     print "connecting to", args.host, "at port", args.port
@@ -29,6 +34,15 @@ def main():
 
     print "setting up device"
     errors = lmg.read_errors()
+
+        # set measuremnt interval
+    lmg.send_short_cmd("CYCL " + str(args.interval));
+
+        # 60Hz low pass
+    if args.lowpass == True:
+        lmg.send_short_cmd("FAAF 0");
+        lmg.send_short_cmd("FILT 4");
+
     lmg.set_ranges(10., 250.)
     lmg.select_values(VAL)
 
@@ -42,10 +56,14 @@ def main():
         while True:
             data = lmg.read_values()
             i += 1
-            sys.stdout.write("\r{0}".format(i))
-            sys.stdout.flush()
             data.insert(0, time.time())
+            if args.verbose:
+                sys.stdout.write(" ".join([ str(x) for x in data ]) + "\n")
+            else:
+                sys.stdout.write("\r{0}".format(i))
+            sys.stdout.flush()
             log.write(" ".join([ str(x) for x in data ]) + "\n")
+            log.flush()
     except KeyboardInterrupt:
         print
      
