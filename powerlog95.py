595da764652155e9ff97c25eb17b8c4771eb8bab
Jan de Cuveland <cuveland@compeng.uni-frankfurt.de>
2024-10-29 15:06:17 +0000

    Add option to write data to InfluxDB

--- powerlog95.py
+++ powerlog95.py
@@ -8,6 +8,13 @@
 
 import argparse, sys, time, lmg95
 
+HAS_INFLUXDB = False
+try:
+    import influxdb
+    HAS_INFLUXDB = True
+except ImportError:
+    pass
+
 VAL = b"count sctc cycr utrms itrms udc idc ucf icf uff iff p pf freq".split()
 
 def main():
@@ -17,17 +24,26 @@ def main():
     parser.add_argument("logfile", help = "Log file name")
     parser.add_argument("-p", "--port", dest = "port", type = int, default=2001,
                         help = "TCP port of RS232-Ethernet converter")
-    parser.add_argument("-v", "--verbose", dest = "verbose", type = int, default=0, help = "Verbose")
+    parser.add_argument("-v", "--verbose", dest = "verbose", action="store_true", default=False,
+                        help = "Verbose")
     parser.add_argument("-l", "--lowpass", dest = "lowpass", action="store_true", default=False,
                         help = "Enable 60 Hz low pass filter")
     parser.add_argument("-i", "--interval", dest="interval", type=float, default=0.5,
                         help = "Measurement interval in seconds")
+    if HAS_INFLUXDB:
+        parser.add_argument("-I", "--influxdb", dest="influxdb", action="store_true", default=False,
+                            help = "Write data to InfluxDB")
     args = parser.parse_args()
+
+    if HAS_INFLUXDB and args.influxdb:
+        influx = influxdb.InfluxDBClient(database="powerlog")
+        influx.create_database("powerlog")
 
     print("connecting to", args.host, "at port", args.port)
     lmg = lmg95.lmg95(args.host, args.port)
 
     print("performing device reset")
+    lmg.cont_off()
     lmg.reset()
 
     print("device found:", lmg.read_id()[1])
@@ -64,6 +80,19 @@ def main():
             sys.stdout.flush()
             log.write(" ".join([ str(x) for x in data ]) + "\n")
             log.flush()
+            if HAS_INFLUXDB and args.influxdb:
+                item = {
+                    "measurement": "powerlog",
+                    "tags": {},
+                    "time" : int(data[0] * 1000),
+                    "fields": dict(zip([x.decode('ascii') for x in VAL], data[1:]))
+                }
+                # Set value to None if it is 9.91e+37 (NaN),
+                # 9.9E+37 (Infinity), or -9.9e+37 (-Infinity)
+                for key in item["fields"]:
+                    if item["fields"][key] in [9.91e+37, 9.9E+37, -9.9e+37]:
+                        item["fields"][key] = None
+                influx.write_points([item], time_precision="ms")
     except KeyboardInterrupt:
         print
 
